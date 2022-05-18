6fa62258e3f67bdc9c5733549dd1a38d5872bf85
Pavel Makarenko <cryfall@gmail.com>
2022-05-18 22:33:04 +0400

    removed files and added gitignore

--- lesson_4/target/debug/deps/lesson_4-85b565b7166367e7.d
+++
@@ -1,5 +0,0 @@
-/Users/m1ome/Work/Home/otus_rust/lesson_4/target/debug/deps/lesson_4-85b565b7166367e7.rmeta: src/main.rs
-
-/Users/m1ome/Work/Home/otus_rust/lesson_4/target/debug/deps/lesson_4-85b565b7166367e7.d: src/main.rs
-
-src/main.rs:
--- lesson_4/target/debug/deps/lesson_4-fa09ba6547fc627e.d
+++
@@ -1,5 +0,0 @@
-/Users/m1ome/Work/Home/otus_rust/lesson_4/target/debug/deps/lesson_4-fa09ba6547fc627e.rmeta: src/main.rs
-
-/Users/m1ome/Work/Home/otus_rust/lesson_4/target/debug/deps/lesson_4-fa09ba6547fc627e.d: src/main.rs
-
-src/main.rs:
